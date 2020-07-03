2d5676da6257168c3a7d12970ba539c2c1154148
jysique <joseysique@gmail.com>
2020-07-03 11:03:00 -0500

    tweens

--- platforms/android/app/src/main/assets/www/js/states/Game.js
+++ platforms/android/app/src/main/assets/www/js/states/Game.js
@@ -4,7 +4,7 @@ Game.prototype = {
     init:function(){
         this.index = this.game.rnd.integerInRange(0, 9);
         this.game.stage.backgroundColor = '#FFFFFF';
-        this.score = 5;
+        this.score = 20;
     },
     create:function(){
         this.initButtons();
@@ -18,37 +18,51 @@ Game.prototype = {
         this.lowerbtn.anchor.setTo(0.5);
         this.lowerbtn.inputEnabled = true;
         this.lowerbtn.events.onInputDown.add(this.lowerAction,this);
-        this.numbers = this.game.add.sprite(200, 200,'numbers',this.index);
+
+        this.old_number = this.game.add.sprite(200, 200,'numbers',this.index);
         this.indexOld = this.index;
-        this.numbers.anchor.setTo(0.5);
+        this.old_number.anchor.setTo(0.5);
     },
     update:function(){
         if(this.score <= 0){
             this.state.start("Gameover");
         }
     },
+    tweenAction(){
+        this.new_number = this.game.add.sprite(400, 200,'numbers',this.indexCurrent);
+        this.new_number.anchor.setTo(0.5);
+        this.tween = this.game.add.tween(this.old_number);
+        this.tween.to({ x: [100], y: [200] }, 300, "Linear");
+        this.tween.start();
+
+        this.tween2 = this.game.add.tween(this.new_number);
+        this.tween2.to({ x: [200], y: [200] }, 300, "Linear");
+        this.tween2.start();
+    },
     higherAction:function(){
         console.log("higher");
         this.indexCurrent = this.game.rnd.integerInRange(0, 9);
-        this.numbers.loadTexture('numbers',this.indexCurrent);
+        this.tweenAction();
         if(this.indexCurrent>this.indexOld){
             this.score+=10;
         }else{
             this.score-= 5;
         }
         console.log(this.indexOld," ; ", this.indexCurrent, " =>score:", this.score);
+        this.old_number = this.new_number;
         this.indexOld = this.indexCurrent;
     },
     lowerAction:function(){
         console.log("lower");
         this.indexCurrent = this.game.rnd.integerInRange(0, 9);
-        this.numbers.loadTexture('numbers',this.indexCurrent);
+        this.tweenAction();
         if(this.indexCurrent < this.indexOld){
             this.score+=10;
         }else{
             this.score-= 5;
         }
         console.log(this.indexOld," ; ", this.indexCurrent, " =>score:", this.score);
+        this.old_number = this.new_number;
         this.indexOld = this.indexCurrent;
     },
 }
